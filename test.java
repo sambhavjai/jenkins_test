ebbae724a103e7e66195bda54da3b923a17d3193
sambhavjai <38015768+sambhavjai@users.noreply.github.com>
2020-05-31 01:45:38 +0530

    c

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test{
     public static void main(String args[])
     {
-        System.out.println("Hello World");
+        //System.out.println("Hello World");
         for(int i=0;i<10;i++)
         {
             System.out.println(i);
